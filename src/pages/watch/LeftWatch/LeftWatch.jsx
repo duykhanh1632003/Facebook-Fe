29071f30ce113821409163f9681a5d6fdcb2c50b
duykhanh1632003 <duykhanh1632003@gmail.com>
2024-07-16 10:11:11 +0700

    video

---
+++ src/pages/watch/LeftWatch/LeftWatch.jsx
@@ -0,0 +1,23 @@
+import { RiSettings5Fill } from "react-icons/ri";
+import { PiVideoFill } from "react-icons/pi";
+
+const LeftWatch = () => {
+  return (
+    <div className="left-sidebar-friend w-[360px] pt-[12px] bg-[#FFFFFF] h-full mt-2 fixed">
+      <div className="flex justify-between pl-[17px] pr-[17px] mb-[12px]">
+        <div className="text-2xl font-bold">Video</div>
+        <div className="w-[36px] h-[36px] rounded-full text-2xl bg-[#E4E6EB] flex items-center justify-center">
+          <RiSettings5Fill />
+        </div>
+      </div>
+      <div className="ml-2 mr-2 pt-1 pb-1 pl-2 pr-2 rounded-lg bg-[#F0F2F5] flex h-[47px] cursor-pointer">
+        <div className="bg-[#1877F2] w-[36px] h-[36px] rounded-full text-2xl flex items-center justify-center text-[#EBE4C9]">
+          <PiVideoFill />
+        </div>
+        <div className="flex items-center ml-2 font-medium">Trang chủ</div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftWatch;
--- src/pages/watch/WatchContainer.jsx
+++ src/pages/watch/WatchContainer.jsx
@@ -1,63 +1,9 @@
-import React, { useRef } from "react";
-import videojs from "video.js";
-import "videojs-contrib-quality-levels";
-import { LinkVideo } from "../../util/linkVideo";
-import VideoJS from "./VideoJS";
-import "./WatchContainer.css"; // Import the CSS file
+import LeftWatch from "./LeftWatch/LeftWatch";
 
 const WatchContainer = () => {
-  const videoUrl = LinkVideo();
-  const videoConRef = useRef(null);
-  const playerRef = useRef(null);
-
-  const handlePlayerReady = (player) => {
-    playerRef.current = player;
-    player.on("waiting", () => {
-      videojs.log("player is waiting");
-    });
-
-    player.on("dispose", () => {
-      videojs.log("player will dispose");
-    });
-  };
-
-  const videoOptions = {
-    autoplay: false,
-    controls: true,
-    responsive: true,
-    fluid: true,
-    playbackRates: [0.5, 0.75, 1, 1.5, 1.75, 2],
-    sources: [
-      {
-        src: videoUrl,
-        type: "video/mp4",
-      },
-    ],
-    controlBar: {
-      children: [
-        "playToggle",
-        "volumePanel",
-        "progressControl",
-        "currentTimeDisplay",
-        "timeDivider",
-        "durationDisplay",
-        "playbackRateMenuButton", // Add playback rate control
-        "pictureInPictureToggle",
-        "qualitySelector",
-        "fullscreenToggle",
-      ],
-      durationDisplay: {
-        timeToShow: ["duration"],
-        countDown: false,
-      },
-    },
-  };
-
   return (
-    <div className="video-player">
-      <div className="video-container" ref={videoConRef}>
-        <VideoJS options={videoOptions} onReady={handlePlayerReady} />
-      </div>
+    <div>
+      <LeftWatch />
     </div>
   );
 };
